c33edc640a6f76200e920161c3721800c7e27319
Kaushal Krishna <132030841+kaushal-krishna@users.noreply.github.com>
2024-03-15 00:04:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const axios = require("axios");
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cron = require("node-cron");
 const fs = require("fs");
 
 const app = express();
@@ -135,7 +134,15 @@ MongoClient.connect(mongoURL, {
       }
     };
 
-    cron.schedule("0 0 0 * * *", getTopTracks);
+    app.get("/spotify/get_top_tracks", async (req, res) => {
+      try {
+        getTopTracks();
+        console.log("Done getting top tracks")
+      } catch (error) {
+        console.error("Error handling request:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
 
     app.get("/spotify/top_tracks", async (req, res) => {
       try {
