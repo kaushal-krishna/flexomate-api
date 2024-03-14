aa8267aa4ca88769e4eaaaa5998c223e0d8b2c04
Kaushal Krishna <132030841+kaushal-krishna@users.noreply.github.com>
2024-03-14 12:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,310 +1,313 @@
-const express = require('express');
-const axios = require('axios');
-const {
-  MongoClient,
-  ObjectId
-} = require('mongodb');
+const express = require("express");
+const axios = require("axios");
+const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const schedule = require('node-schedule');
+const bodyParser = require("body-parser");
+const cron = require("node-cron");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 const PORT_USERS_API = 3001;
 const PORT_COLLECTIONS_API = 3002;
-const mongoURL = 'mongodb+srv://florixer:Kau93043@flexomate-cluster.bzqxpj3.mongodb.net/flexomate_db?retryWrites=true&w=majority';
+const mongoURL =
+  "mongodb+srv://florixer:Kau93043@flexomate-cluster.bzqxpj3.mongodb.net/flexomate_db?retryWrites=true&w=majority";
 
 app.use(cors());
 app.use(bodyParser.json());
 let db;
 
-schedule.scheduleJob('0 0 * * *', () => {
-  getTopTracks();
-});
-let playlistTracks;
-let printError;
-
-const getTopTracks = async () => {
-  try {
-    const {
-      data
-    } = await axios.get(
-      "https://spotify-web2.p.rapidapi.com/playlist_tracks/",
-      {
-        params: {
-          id: "37i9dQZEVXbMWDif5SCBJq",
-          limit: "30",
-        },
-        headers: {
-          "X-RapidAPI-Key": "b2fd7811a0msha2d668dfd2c45bap100a30jsn22bc61f46071",
-          "X-RapidAPI-Host": "spotify-web2.p.rapidapi.com",
-        },
-      }
-    );
-    playlistTracks = data.items;
-  } catch (error) {
-    printError = `${error.code} : ${error.message}`;
-  }
-};
-
 // Connect to MongoDB
 MongoClient.connect(mongoURL, {
-  useNewUrlParser: true, useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-.then((client) => {
-  console.log('Connected to MongoDB');
-  db = client.db('flexomate_db');
+  .then((client) => {
+    console.log("Connected to MongoDB");
+    db = client.db("flexomate_db");
 
-  // Users API - CRUD Operations
+    // Users API - CRUD Operations
 
-  // Create a new user
-  app.post('/users', async (req, res) => {
-    try {
-      await db.collection('users').insertOne(req.body);
-      res.status(200).json({
-        message: 'User creation initiated!'
-      });
-    } catch (error) {
-      console.error('Error creating user:', error.message);
-      res.status(500).json({
-        message: 'User creation failed.'
-      });
-    }
-  });
-
-  // Get all users
-  app.get('/users', async (req, res) => {
-    try {
-      const users = await db.collection('users').find().toArray();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // Get user by username
-  app.get('/users/:username', async (req, res) => {
-    const {
-      username
-    } = req.params;
-    try {
-      const user = await db.collection('users').findOne({
-        username
-      });
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // Search users by partial username match
-  app.get('/users/search/:partialUsername', async (req, res) => {
-    const {
-      partialUsername
-    } = req.params;
-    try {
-      const regex = new RegExp(partialUsername, 'i');
-      const users = await db.collection('users').find({
-        username: {
-          $regex: regex
-        }
-      }).toArray();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // Update user information
-  app.patch('/users/:id', async (req, res) => {
-    const {
-      id
-    } = req.params;
-    const {
-      username, email
-    } = req.body;
-    try {
-      const result = await db.collection('users').findOneAndUpdate(
-        {
-          _id: new ObjectId(id)
-        },
-        {
-          $set: {
-            username, email
-          }
-        },
-        {
-          returnDocument: 'after'
-        }
-      );
-      res.json({
-        message: 'User Updated Successfully!'
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // Delete user by ID
-  app.delete('/users/:id', async (req, res) => {
-    const {
-      id
-    } = req.params;
-    try {
-      const result = await db.collection('users').findOneAndDelete({
-        _id: new ObjectId(id)
-      });
-      res.json({
-        message: 'User Deleted Successfully!'
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // Collections API - CRUD Operations
-
-  // Create a new library
-  app.post('/libraries', async (req, res) => {
-    try {
-      await db.collection('libraries').insertOne(req.body);
-      res.status(200).json({
-        message: 'Library creation initiated!'
-      });
-    } catch (error) {
-      console.error('Error creating library:', error.message);
-      res.status(500).json({
-        message: 'Library creation failed.'
-      });
-    }
-  });
-
-  // Get all libraries
-  app.get('/libraries', async (req, res) => {
-    try {
-      const libraries = await db.collection('libraries').find().toArray();
-      res.status(200).json(libraries);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // Get library by ID
-  app.get('/libraries/:id', async (req, res) => {
-    const {
-      id
-    } = req.params;
-    try {
-      const library = await db.collection('libraries').findOne({
-        _id: new ObjectId(id)
-      });
-
-      if (!library) {
-        return res.status(404).json({
-          message: 'Library not found'
+    // Create a new user
+    app.post("/users", async (req, res) => {
+      try {
+        await db.collection("users").insertOne(req.body);
+        res.status(200).json({
+          message: "User creation initiated!",
+        });
+      } catch (error) {
+        console.error("Error creating user:", error.message);
+        res.status(500).json({
+          message: "User creation failed.",
         });
       }
+    });
 
-      res.status(200).json(library);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    }
-  });
-
-  // SpotifyDown API Download request
-  app.get('/spotify/download/:id',
-    async (req, res) => {
+    // Get all users
+    app.get("/users", async (req, res) => {
       try {
-        const {
-          id
-        } = req.params;
-        const externalApiResponse = await axios.get(`https://api.spotifydown.com/download/${id}`, {
-          headers: {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-            'Origin': 'https://spotifydown.com',
-            'Referer': 'https://spotifydown.com/',
-            'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120"',
-            'Sec-Ch-Ua-Mobile': '?1',
-            'Sec-Ch-Ua-Platform': '"Android"',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-          },
+        const users = await db.collection("users").find().toArray();
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
         });
+      }
+    });
+
+    // Get user by username
+    app.get("/users/:username", async (req, res) => {
+      const { username } = req.params;
+      try {
+        const user = await db.collection("users").findOne({
+          username,
+        });
+        res.json(user);
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // Search users by partial username match
+    app.get("/users/search/:partialUsername", async (req, res) => {
+      const { partialUsername } = req.params;
+      try {
+        const regex = new RegExp(partialUsername, "i");
+        const users = await db
+          .collection("users")
+          .find({
+            username: {
+              $regex: regex,
+            },
+          })
+          .toArray();
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // Update user information
+    app.patch("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const { username, email } = req.body;
+      try {
+        const result = await db.collection("users").findOneAndUpdate(
+          {
+            _id: new ObjectId(id),
+          },
+          {
+            $set: {
+              username,
+              email,
+            },
+          },
+          {
+            returnDocument: "after",
+          },
+        );
+        res.json({
+          message: "User Updated Successfully!",
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // Delete user by ID
+    app.delete("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      try {
+        const result = await db.collection("users").findOneAndDelete({
+          _id: new ObjectId(id),
+        });
+        res.json({
+          message: "User Deleted Successfully!",
+        });
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // Collections API - CRUD Operations
+
+    // Create a new library
+    app.post("/libraries", async (req, res) => {
+      try {
+        await db.collection("libraries").insertOne(req.body);
+        res.status(200).json({
+          message: "Library creation initiated!",
+        });
+      } catch (error) {
+        console.error("Error creating library:", error.message);
+        res.status(500).json({
+          message: "Library creation failed.",
+        });
+      }
+    });
+
+    // Get all libraries
+    app.get("/libraries", async (req, res) => {
+      try {
+        const libraries = await db.collection("libraries").find().toArray();
+        res.status(200).json(libraries);
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // Get library by ID
+    app.get("/libraries/:id", async (req, res) => {
+      const { id } = req.params;
+      try {
+        const library = await db.collection("libraries").findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!library) {
+          return res.status(404).json({
+            message: "Library not found",
+          });
+        }
+
+        res.status(200).json(library);
+      } catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+    // SpotifyDown API Download request
+    app.get("/spotify/download/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const externalApiResponse = await axios.get(
+          `https://api.spotifydown.com/download/${id}`,
+          {
+            headers: {
+              Accept: "*/*",
+              "Accept-Encoding": "gzip, deflate, br",
+              "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+              Origin: "https://spotifydown.com",
+              Referer: "https://spotifydown.com/",
+              "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120"',
+              "Sec-Ch-Ua-Mobile": "?1",
+              "Sec-Ch-Ua-Platform": '"Android"',
+              "Sec-Fetch-Dest": "empty",
+              "Sec-Fetch-Mode": "cors",
+              "Sec-Fetch-Site": "same-site",
+              "User-Agent":
+                "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+            },
+          },
+        );
 
         // Send the external API response as your API response
         res.json(externalApiResponse.data);
       } catch (error) {
-        console.error('Error making external API request:', error.message);
+        console.error("Error making external API request:", error.message);
         res.status(500).json({
-          error: 'Internal Server Error'
+          error: "Internal Server Error",
         });
       }
     });
-  app.get('/spotify/top_tracks',
-    async (req, res) => {
-        res.status(200).json({playlistTracks});
-    });
+    const getTopTracks = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://spotify-downloader1.p.rapidapi.com/trackList/playlist/37i9dQZEVXbLZ52XmnySJg",
+          {
+            headers: {
+              "X-RapidAPI-Key":
+                "b2fd7811a0msha2d668dfd2c45bap100a30jsn22bc61f46071",
+              "X-RapidAPI-Host": "spotify-downloader1.p.rapidapi.com",
+            },
+          },
+        );
+        // Truncate the existing file
+        fs.truncate("json/top_tracks.json", (err) => {
+          if (err) throw err;
+          console.log("json/top_tracks.json was truncated ✓");
+        });
+        // Write data to file
+        fs.writeFile("json/top_tracks.json", JSON.stringify(data), (err) => {
+          if (err) {
+            console.error("Error writing to file:", err);
+            return; // Stop execution if error occurs
+          }
+        });
+        console.log("Done Updating Top Tracks ✓");
+      } catch (error) {
+        console.error("Error retrieving top tracks:", error);
+        // Handle the error here (e.g., retry, send notification, etc.)
+      }
+    };
 
-  // Root endpoint
-  app.get('/',
-    async (req, res) => {
+    // Schedule your cron job and pass the function
+    cron.schedule("0 0 0 * * *", getTopTracks);
+
+    app.get("/spotify/top_tracks", async (req, res) => {
+      try {
+        // Read the top tracks data from the JSON file
+        fs.readFile("json/top_tracks.json", function (err, data) {
+          if (err) {
+            console.error("Error reading file:", err);
+            res.status(500).json({ message: "Failed to retrieve top tracks" });
+            return;
+          }
+          // Parse JSON data
+          const topTracks = JSON.parse(data);
+          // Respond with the top tracks data
+          res.status(200).json({ topTracks });
+        });
+      } catch (error) {
+        console.error("Error handling request:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+    // Root endpoint
+    app.get("/", async (req, res) => {
       res.status(200).json({
-        message: 'API Server is working fine!'
+        message: "API Server is working fine!",
       });
     });
 
-  // Error handling middleware
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-      message: 'Something went wrong!'
+    // Error handling middleware
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).json({
+        message: "Something went wrong!",
+      });
     });
-  });
-  let serverHasMounted = false;
-  app.listen(PORT, () => {
-  if (!serverHasMounted) {
-    // Execute your one-time code here
-    getTopTracks();
-    // Set the flag to ensure this code runs only once
-    serverHasMounted = true;
-  }
 
-  console.log(`Server is running on port ${PORT}`);
-});
+    // Default API Server
+    app.listen(PORT, () => {
+      console.log(`API Server is running on port ${PORT}`);
+    });
 
-  // Users API Server
-  app.listen(PORT_USERS_API,
-    () => {
+    // Users API Server
+    app.listen(PORT_USERS_API, () => {
       console.log(`Users API Server is running on port ${PORT_USERS_API}`);
     });
 
-  // Collections API Server
-  app.listen(PORT_COLLECTIONS_API,
-    () => {
-      console.log(`Collections API Server is running on port ${PORT_COLLECTIONS_API}`);
+    // Collections API Server
+    app.listen(PORT_COLLECTIONS_API, () => {
+      console.log(
+        `Collections API Server is running on port ${PORT_COLLECTIONS_API}`,
+      );
     });
-
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:',
-    err);
-});
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
