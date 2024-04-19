bd7d60d2497ad0e03e1c3fb225337304bb4baad6
kaushal-krishna <kaushalkrishna011@gmail.com>
2024-04-19 20:13:49 +0530

    'Update'

--- mailer/sendEmailOtp.js
+++ mailer/sendEmailOtp.js
@@ -1,11 +1,14 @@
 import nodemailer from "nodemailer";
+// import email and password from .env
+require("dotenv").config();
+const { EMAIL, PASSWORD } = process.env;
 
 const transporter = nodemailer.createTransport({
   host: "smtp.ethereal.email",
   port: 587,
   secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
-    user: "",
-    pass: "jn7jnAPss4f63QBp6D",
+    user: EMAIL,
+    pass: PASSWORD,
   },
 });
