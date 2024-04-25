bae6f3b1d21dc4f3c1e3d3458b85bad316a15941
Kaushal Krishna <admin@florixer.rf.gd>
2024-04-25 19:32:53 +0530

    Update

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cron = require("node-cron");
-const fs = require("fs");
 const { getAllUsers, searchUsers, createUserAccount, deleteUserAccount } = require("./routes/users/userAccountProcessor.js")
+const { spotifyGetTopTracks, spotifyListTopTracks, spotifyDownloadTrack } =
+require("./routes/music/spotifyProcessor.js")
 const { sendSignupEmailOtp, verifySignupEmailOtp } = require("./routes/mailer/otpProcessor.js");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -78,101 +79,13 @@ MongoClient.connect(mongoURI, {
     app.post("/users/create_account", createUserAccount);
     app.get("/users/delete_account/:username", deleteUserAccount);
 
-    app.get("/spotify/download/:id", async (req, res) => {
-      try {
-        const { id } = req.params;
-        const externalApiResponse = await axios.get(
-          `https://api.spotifydown.com/download/${id}`,
-          {
-            headers: {
-              Accept: "*/*",
-              "Accept-Encoding": "gzip, deflate, br",
-              "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-              Origin: "https://spotifydown.com",
-              Referer: "https://spotifydown.com/",
-              "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120"',
-              "Sec-Ch-Ua-Mobile": "?1",
-              "Sec-Ch-Ua-Platform": '"Android"',
-              "Sec-Fetch-Dest": "empty",
-              "Sec-Fetch-Mode": "cors",
-              "Sec-Fetch-Site": "same-site",
-              "User-Agent":
-                "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
-            },
-          },
-        );
+    app.get("/spotify/download/:id", spotifyDownloadTrack);
 
-        res.json(externalApiResponse.data);
-      } catch (error) {
-        console.error("Error making external API request:", error.message);
-        res.status(500).json({
-          error: "Internal Server Error",
-        });
-      }
-    });
+    cron.schedule("0 0 0 * * *", spotifyGetTopTracks);
 
-    const getTopTracks = async () => {
-      try {
-        const { data } = await axios.get(
-          "https://spotify-downloader1.p.rapidapi.com/trackList/playlist/37i9dQZEVXbLZ52XmnySJg",
-          {
-            headers: {
-              "X-RapidAPI-Key":
-                "b2fd7811a0msha2d668dfd2c45bap100a30jsn22bc61f46071",
-              "X-RapidAPI-Host": "spotify-downloader1.p.rapidapi.com",
-            },
-          },
-        );
-        fs.truncate("json/top_tracks.json", (err) => {
-          if (err) throw err;
-          console.log("json/top_tracks.json was truncated");
-        });
-        fs.writeFile("json/top_tracks.json", JSON.stringify(data), (err) => {
-          if (err) {
-            console.error("Error writing to file:", err);
-            return;
-          }
-        });
-        console.log("Done Updating Top Tracks");
-      } catch (error) {
-        console.error("Error retrieving top tracks:", error);
-      }
-    };
+    app.get("/spotify/top_tracks", spotifyListTopTracks);
 
-    cron.schedule("0 0 0 * * *", getTopTracks);
-
-    app.get("/spotify/top_tracks", async (req, res) => {
-      try {
-        fs.readFile("json/top_tracks.json", function (err, data) {
-          if (err) {
-            console.error("Error reading file:", err);
-            res.status(500).json({
-              message: "Failed to retrieve top tracks",
-            });
-            return;
-          }
-          const topTracks = JSON.parse(data);
-          res.status(200).json({ topTracks });
-        });
-      } catch (error) {
-        console.error("Error handling request:", error);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    });
-
-    app.get("/spotify/get_top_tracks", async (req, res) => {
-      try {
-        getTopTracks();
-        res.status(200).json({
-          message: "getTopTracks() has been invoked",
-        });
-      } catch (error) {
-        console.error("Error handling request:", error);
-        res.status(500).json({
-          message: "Couldn't invoke getTopTracks()",
-        });
-      }
-    });
+    app.get("/spotify/get_top_tracks", spotifyGetTopTracks)
     app.post("/mailer/send_signup_email_otp", sendSignupEmailOtp);
     app.post("/mailer/verify_signup_email_otp", verifySignupEmailOtp);
   })
---
+++ routes/music/spotifyProcessor.js
@@ -0,0 +1,84 @@
+const fs = require("fs").promises;
+const path = require("path");
+const axios = require("axios");
+const topTracksJsonFilePath = path.join(__dirname, "../../json/top_tracks.json");
+const spotifyListTopTracks = async (req, res) => {
+  try {
+    const data = await fs.readFile(topTracksJsonFilePath, "utf8");
+    const topTracks = JSON.parse(data);
+    res.status(200).json({ topTracks });
+  } catch (error) {
+    console.error("Error handling request:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+const spotifyGetTopTracks = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://spotify-downloader1.p.rapidapi.com/trackList/playlist/37i9dQZEVXbLZ52XmnySJg",
+      {
+        headers: {
+          "X-RapidAPI-Key":
+            "b2fd7811a0msha2d668dfd2c45bap100a30jsn22bc61f46071",
+          "X-RapidAPI-Host": "spotify-downloader1.p.rapidapi.com",
+        },
+      },
+    );
+    fs.truncate(topTracksJsonFilePath, (err) => {
+      if (err) throw err;
+      console.log("topTracksJsonFilePath was truncated");
+    });
+    fs.writeFile(topTracksJsonFilePath, JSON.stringify(data), (err) => {
+      if (err) {
+        console.error("Error writing to file:", err);
+        return;
+      }
+    });
+    console.log("Done Updating Top Tracks");
+    res.status(200).json({
+      message: "getTopTracks request has been invoked",
+    });
+  } catch (error) {
+    console.error("Error handling getTopTracks Request:", error);
+    res.status(500).json({
+      message: "Couldn't invoke getTopTracks request",
+    });
+  }
+};
+const spotifyDownloadTrack = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const externalApiResponse = await axios.get(
+      `https://api.spotifydown.com/download/${id}`,
+      {
+        headers: {
+          Accept: "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+          Origin: "https://spotifydown.com",
+          Referer: "https://spotifydown.com/",
+          "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120"',
+          "Sec-Ch-Ua-Mobile": "?1",
+          "Sec-Ch-Ua-Platform": '"Android"',
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-site",
+          "User-Agent":
+            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+        },
+      },
+    );
+
+    res.json(externalApiResponse.data);
+  } catch (error) {
+    res.status(500).json({
+      error: "Unable to make download request",
+    });
+  }
+};
+
+module.exports = {
+  spotifyGetTopTracks,
+  spotifyListTopTracks,
+  spotifyDownloadTrack
+}
